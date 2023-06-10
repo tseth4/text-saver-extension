4459947d793503b12d9367e434b0afd23395bc5b
ts <tristansetha@gmail.com>
2023-06-10 09:00:57 -0700

    Update (not working)

--- scripts/content.js
+++ scripts/content.js
@@ -1,14 +1,30 @@
 function highlightHandler(e) {
-  var text = document.getSelection();
-  if(text !== '') {
-      doStuff(text);
+  var selection = document.getSelection();
+  let selectedText = selection.toString();
+  if (selection !== '') {
+    console.log("className: ", selection.anchorNode);
+    // let temp_el = document.createElement("div");
+    // temp_el.innerHTML = "Hello World!";
+    // selection.anchorNode.appendChild(temp_el);
+    selectionHelper(selection);
   }
 }
 
-function doStuff(text) {
-  console.log("text: ", text.focusNode.data);
+function selectionHelper(selection) {
+  console.log("selection: ", selection);
+  selection.focusNode.parentNode.innerHTML = "HEllo World!";
+  let activeElement = document.activeElement;
+  var start = selection.anchorOffset;
+  var end = selection.focusOffset;
+  if (start >= 0 && end >= 0) {
+    let basenodedata = selection.baseNode.data;
+    console.log(start, end);
+    console.log("mystr: ", basenodedata);
+    console.log("activeEl: ", activeElement);
+    console.log("offset: ", basenodedata.substring(start, end))
+  }
 }
 
 document.onmouseup = highlightHandler;
 
-console.log("from content")
+console.log("from content");
